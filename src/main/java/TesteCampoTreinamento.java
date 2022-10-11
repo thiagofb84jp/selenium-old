0f6ac6d45a7fcdbb095cbe1be2732e9b4b927ebc
Thiago Ferreira <thiagoferreira@NATURAL-012839.local>
2022-10-11 17:40:54 -0300

    Including more artifacts related to Selenium training

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,16 +1,18 @@
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class TesteCampoTreinamento {
 
+    String nome = "Teste de escrita";
+
     @Test
     public void testeTextField() {
-        String nome = "Teste de escrita";
-
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
@@ -23,6 +25,119 @@ public class TesteCampoTreinamento {
 
     @Test
     public void deveInteragirComTextArea() {
-        
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("elementosForm:sugestoes")).sendKeys(nome);
+        Assert.assertEquals(nome, driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+
+        driver.quit();
+    }
+
+    @Test
+    public void deveInteragirComRadioButton() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+
+        driver.quit();
+    }
+
+    @Test
+    public void deveInteragirComCheckbox() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+        Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+
+        driver.quit();
+    }
+
+    @Test
+    public void deveVerificarValoresCombo() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+        Select combo = new Select(element);
+        List<WebElement> options = combo.getOptions();
+        Assert.assertEquals(8, options.size());
+
+        boolean encontrou = false;
+        for (WebElement option : options) {
+            if (option.getText().equals("Mestrado")) {
+                encontrou = true;
+                break;
+            }
+        }
+        Assert.assertTrue(encontrou);
+
+        driver.quit();
+    }
+
+    @Test
+    public void deveVerificarValoresComboMultiplo() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+        Select combo = new Select(element);
+        combo.selectByVisibleText("Natacao");
+        combo.selectByVisibleText("Corrida");
+        combo.selectByVisibleText("O que eh esporte?");
+
+        List<WebElement> allSelectedOptions = combo.getAllSelectedOptions();
+        Assert.assertEquals(3, allSelectedOptions.size());
+
+        combo.deselectByVisibleText("Corrida");
+        allSelectedOptions = combo.getAllSelectedOptions();
+        Assert.assertEquals(2, allSelectedOptions.size());
+
+        driver.quit();
+    }
+
+    @Test
+    public void deveInteragirComBotoes() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        WebElement botao = driver.findElement(By.id("buttonSimple"));
+        botao.click();
+
+        Assert.assertEquals("Obrigado!", botao.getAttribute("value"));
+
+        driver.quit();
+    }
+
+    @Test
+    @Ignore
+    public void deveInteragirComLinks() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.linkText("Voltar")).click();
+
+        driver.quit();
+    }
+
+    @Test
+    public void deveBuscarTextosNaPagina() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("Campo de Treinamento"));
+
+        driver.quit();
     }
 }
